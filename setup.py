9dc9bc7d5dff4e30bb921a683b465ba967f65efb
Kevin Adler <kadler@us.ibm.com>
2018-11-29 19:59:16 -0600

    Remove option to not build itoollib
    itoollib can now be built on all platforms, so we don't need an
    option to disable it on non-PASE builds. Other platforms will just
    have a dummy module with no functions.

--- setup.py
+++ setup.py
@@ -1,37 +1,6 @@
 from setuptools import setup, find_packages
 from distutils.core import Extension
-from distutils.command.build_ext import build_ext
-from distutils.command.build_py import build_py
 
-disable_libcall = False
-
-class itoolkit_build_ext(build_ext):
-    user_options = build_ext.user_options + [
-        ('disable-libcall', None, 'Disable support for iLibCall'),
-        ]
-    
-    def initialize_options(self):
-        self.disable_libcall = False
-        build_ext.initialize_options(self)
-    
-    def finalize_options(self):
-        build_ext.finalize_options(self)
-        
-        global disable_libcall
-        disable_libcall = self.disable_libcall
-        
-        # If the user doesn't want libcall, don't build it
-        if self.disable_libcall:
-            del self.extensions[:]
-
-class itoolkit_build_py(build_py):
-    def finalize_options(self):
-        build_py.finalize_options(self)
-        
-        # If the user doesn't want libcall, don't ship the package
-        if disable_libcall:
-            self.packages.remove('itoolkit.lib')
-          
 setup(
   name='itoolkit',
   version='1.5.1',
@@ -42,7 +11,6 @@ setup(
   author_email='adc@us.ibm.com',
   license='BSD',
   zip_safe=False,
-  cmdclass={"build_ext": itoolkit_build_ext, 'build_py': itoolkit_build_py},
   packages=find_packages(),
   data_files = [ ('itoolkit', ['itoolkit/README']),
                  ('itoolkit', ['itoolkit/LICENSE']), 

50341cb1155bb8f4361490f8414a32739e78dd3d
Kevin Adler <kadler@us.ibm.com>
2018-11-29 23:10:47 -0600

    Add Trove Classifiers
    See https://pypi.org/classifiers/ There's probably more we could add

--- setup.py
+++ setup.py
@@ -15,4 +15,13 @@ setup(
     ext_modules=[
         Extension('itoolkit/itoollib', ['itoolkit/lib/itoollib.c'])
     ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
